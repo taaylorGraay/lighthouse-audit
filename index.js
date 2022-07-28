8816c5bd313a89de9cf58e76196bbdfd62cc3448
Taylor Gray <taaylorgraay@gmail.com>
2022-07-28 18:02:30 +0100

    run lighthouse audits with minimal variance

--- index.js
+++ index.js
@@ -1,105 +1,55 @@
 import fs from 'fs';
-import open from 'open';
 import puppeteer from 'puppeteer';
 import { startFlow } from 'lighthouse/lighthouse-core/fraggle-rock/api.js';
 
 async function captureReport() {
 
-  //Angular Audit
 
-  const aBrowser = await puppeteer.launch({ headless: false });
-  const aPage = await aBrowser.newPage();
 
-  const aTestUrl = 'https://bwm-next-generation-new.herokuapp.com/';
-  const aFlow = await startFlow(aPage, { name: 'Cold and warm navigations' });
-  await aFlow.navigate(aTestUrl, {
-    stepName: 'Cold navigation'
-  });
-  await aFlow.navigate(aTestUrl, {
-    stepName: 'Warm navigation 1',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await aFlow.navigate(aTestUrl, {
-    stepName: 'Warm navigation 2',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await aFlow.navigate(aTestUrl, {
-    stepName: 'Warm navigation 3',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await aFlow.navigate(aTestUrl, {
-    stepName: 'Warm navigation 4',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await aFlow.navigate(aTestUrl, {
-    stepName: 'Warm navigation 5',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
+  for (let i = 1; i <= 10; i++) {
+    const Browser = await puppeteer.launch({ headless: false });
+    const Page = await Browser.newPage();
 
-  await aBrowser.close();
+    //Angular Audit
 
-  const aReport = await aFlow.generateReport();
-  fs.writeFileSync('angular.report.html', aReport);
-  open('angular.report.html', { wait: false });
+    const aTestUrl = 'https://bwm-next-generation-new.herokuapp.com/';
+    const aFlow = await startFlow(Page, { name: 'Cold and warm navigations' });
+    await aFlow.navigate(aTestUrl, {
+      stepName: 'Cold navigation'
+    });
 
-//////////////////////////////////////////////////////////////
+    await aFlow.navigate(aTestUrl, {
+      stepName: 'Warm navigation ' + i + '',
+      configContext: {
+        settingsOverrides: { disableStorageReset: true },
+      },
+    });
 
-  //React Audit  
+    const aReport = await aFlow.generateReport();
+    fs.writeFileSync('angular-reports/angular.report' + i + '.html', aReport);
 
-  const rBrowser = await puppeteer.launch({ headless: false });
-  const rPage = await rBrowser.newPage();
+    //React Audit  
 
-  const rTestUrl = 'https://bwm-react-redux-new.herokuapp.com/';
-  const rFlow = await startFlow(rPage, { name: 'Cold and warm navigations' });
-  await rFlow.navigate(rTestUrl, {
-    stepName: 'Cold navigation'
-  });
-  await rFlow.navigate(rTestUrl, {
-    stepName: 'Warm navigation 1',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await rFlow.navigate(rTestUrl, {
-    stepName: 'Warm navigation 2',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await rFlow.navigate(rTestUrl, {
-    stepName: 'Warm navigation 3',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await rFlow.navigate(rTestUrl, {
-    stepName: 'Warm navigation 4',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
-  await rFlow.navigate(rTestUrl, {
-    stepName: 'Warm navigation 5',
-    configContext: {
-      settingsOverrides: { disableStorageReset: true },
-    },
-  });
+    const rTestUrl = 'https://bwm-react-redux-new.herokuapp.com/';
+    const rFlow = await startFlow(Page, { name: 'Cold and warm navigations' });
+    await rFlow.navigate(rTestUrl, {
+      stepName: 'Cold navigation'
+    });
 
-  await rBrowser.close();
 
-  const rReport = await rFlow.generateReport();
-  fs.writeFileSync('react.report.html', rReport);
-  open('react.report.html', { wait: false });
+    await rFlow.navigate(rTestUrl, {
+      stepName: 'Warm navigation ' + i + '',
+      configContext: {
+        settingsOverrides: { disableStorageReset: true },
+      },
+    });
+
+    const rReport = await rFlow.generateReport();
+    fs.writeFileSync('react-reports/react.report' + i + '.html', rReport);
+
+    await Browser.close();
+  }
+
 }
 
 captureReport();
